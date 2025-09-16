3e8bb07052135f1aea91bfad7c5ad4a0f43a20bf
Abbas4tech <shaikhabbas54@gmail.com>
2025-09-17 05:10:02 +0530

    ♻️ ref: reframe structure

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -3,10 +3,10 @@ import React, { PropsWithChildren } from "react";
 import Header from "@/components/Header";
 
 const layout = ({ children }: PropsWithChildren): React.JSX.Element => (
-  <div>
+  <>
     <Header />
     {children}
-  </div>
+  </>
 );
 
 export default layout;
