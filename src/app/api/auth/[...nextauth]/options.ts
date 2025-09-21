cc2dbd0fd293a000736444da4301440af5a93048
Abbas4tech <shaikhabbas54@gmail.com>
2025-09-21 22:34:52 +0530

    ✨ feat: refactor code

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -7,6 +7,8 @@ import bcrypt from "bcryptjs";
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User";
 
+import { controlGoogleSignInFlow } from "./controller/google-signin";
+
 export const authOptions: NextAuthOptions = {
   providers: [
     CredentialProvider({
@@ -71,39 +73,7 @@ export const authOptions: NextAuthOptions = {
   callbacks: {
     async signIn({ user, account }) {
       if (account?.provider === "google") {
-        await dbConnect();
-        try {
-          const existingUser = await UserModel.findOne({
-            email: user.email,
-          });
-
-          if (!existingUser) {
-            const newUser = new UserModel({
-              email: user.email,
-              username: user.email?.split("@")[0],
-              isVerified: true,
-              isAcceptingMessages: true,
-              password: await bcrypt.hash(
-                Math.random().toString(36).slice(-12),
-                10
-              ),
-              verifyCode: Math.floor(
-                100000 + Math.random() * 900000
-              ).toString(),
-              verifyCodeExpiry: new Date(Date.now() + 3600000),
-            });
-            const savedUser = await newUser.save();
-            user._id = savedUser._id.toString();
-          } else {
-            user._id = existingUser._id.toString();
-            user.isVerified = existingUser.isVerified;
-            user.isAcceptingMessage = existingUser.isAcceptingMessage;
-            user.username = existingUser.username;
-          }
-        } catch (error) {
-          console.error("Error handling Google sign-in:", error);
-          return false;
-        }
+        return await controlGoogleSignInFlow(user);
       }
       return true;
     },
